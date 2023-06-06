32446ca30b7e1e61538331527b2bd7c271da5b9a
yamkelajojo <mhlengijojo7@gmail.com>
2023-06-06 03:52:27 +0200

    Improved Padding. Added Links to Uno's Discography and meta head name. Considered as Complete

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,16 @@
 export default defineNuxtConfig({
   modules: ['nuxt-icon'],
   css: ["@/assets/css/global.css"],
+  app: {
+    head: {
+      title: "UnoTheAcivist Landing Page",
+      meta: [{ name: "UnoTheAcivist Landing Page", content: "Discography" }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+        },
+      ],
+    },
+  },
 });
