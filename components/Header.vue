fdafa336ffb2dade6c2bcb5b4b60ca34599af7d2
yamkelajojo <mhlengijojo7@gmail.com>
2023-06-06 03:51:32 +0200

    Improved Padding. Added Links to Uno's Discography. Considered as Complete

--- components/Header.vue
+++ components/Header.vue
@@ -1,36 +1,43 @@
 <template>
   <div class="header">
-    <nav>
-      <div class="nav-left">UnotheActivist</div>
-      <div class="nav-right">
-        <ul>
-          <li><a href=""></a>Youtube</li>
-          <li><a href=""></a>Soundcloud</li>
-          <li><a href=""></a>Insta</li>
-        </ul>
-      </div>
-    </nav>
-    <div class="hero">
-      <div class="hero-left">
-        <div id="hero-main-text">This is my favourite rapper</div>
-        <div id="hero-secondary-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          minima sed reiciendis. Ab excepturi cumque error possimus cum!
+    <div class="wrapper">
+      <nav>
+        <div class="nav-left"><a :href=unotheactivist>UnotheActivist</a></div>
+        <div class="nav-right">
+          <ul>
+            <li><a href="https://www.youtube.com/@UnoTheActivist"><Icon  name="mdi:youtube" size="3rem" color="#b2071d"></Icon></a></li>
+            <li><a href="https://soundcloud.com/the-act"><Icon name="mdi:soundcloud" size="3rem" color="#ff7700"></Icon></a></li>
+            <li><a href="https://www.instagram.com/unotheactivist/"><Icon name="mdi:instagram" size="3rem" color="#E1306C"></Icon></a></li>
+          </ul>
         </div>
-        <button>Like</button>
-      </div>
-      <div class="hero-right">
-        <img
-          id="placeholder-img"
-          src="assets\images\cyber_neon_classic_garage.jpg"
-          alt=""
-        />
+      </nav>
+      <div class="hero">
+        <div class="hero-left">
+          <div id="hero-main-text">This is my favourite rapper</div>
+          <div id="hero-secondary-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quis sit officia deserunt eveniet! Magni, voluptatibus!
+          </div>
+          <div>
+            <button>Like</button>
+          </div>
+        </div>
+        <div class="hero-right">
+          <img
+            id="placeholder-img"
+            src="../assets/images/unowallpaper.jpg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const unotheactivist = ref("https://en.wikipedia.org/wiki/UnoTheActivist")
+</script>
 
 <style scoped>
 .header {
@@ -41,7 +48,14 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0.5rem 0;
+  padding: 1.2rem 0;
+}
+
+.hero-left{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1em;
 }
 
 ul {
@@ -53,11 +67,13 @@ ul {
 .nav-left {
   color: #f9faf8;
   font-size: 24px;
-  font-weight: 550;
+  font-weight: 600;
 }
 
 .hero {
   display: flex;
+  padding: 1.6rem 0 5rem 0;
+  gap: 10%
 }
 
 #placeholder-img {
@@ -70,12 +86,21 @@ ul {
   color: #f9faf8;
   font-weight: 800;
 }
+.hero-right{
+  margin: auto;
+}
+
+a{
+  color: #f9faf8;
+
+}
 
 button {
   background-color: #3882f6;
   color: #f9faf8;
-  font-size: 10px;
+  font-size: 12px;
   font-weight: 600;
   border: none;
+  padding: 0.5rem 1.5rem;
 }
 </style>
--- components/Main.vue
+++ components/Main.vue
@@ -1,55 +1,80 @@
 <template>
   <div class="main-section">
     <section class="random-info-block">
-      <h3 id="info-header-text">Best Uno Projects</h3>
+      <div class="wrapper">
+        <h3 id="info-header-text">Best Uno Projects</h3>
+      </div>
       <div class="cards-container">
         <div class="card">
-          <img
-            src="assets\images\392e8758089593730dcd5ca64f38c62a.999x999x1.jpg"
-            alt=""
-          />
-          <p>Project name</p>
+          <a :href=showStopper>
+            <img
+              src="../assets/images/WAYDT.jpg"
+              alt=""
+            />
+            <p>Show Stopper</p>
+          </a>
         </div>
         <div class="card">
-          <img
-            src="assets\images\deaaaddf751e238b3566a430b5b75dbb.1000x1000x1.png"
-            alt=""
-          />
-          <p>Project name</p>
+          <a :href=unoverse>
+            <img src="../assets/images/Unoverse.jpeg" alt="" />
+            <p>Unoverse</p>
+          </a>
         </div>
         <div class="card">
-          <img src="assets\images\Unoverse.jpeg" alt="" />
-          <p>Project name</p>
+          <a :href=unoverse_two>
+            <img
+              src="../assets/images/Unoverse2.png"
+              alt=""
+            />
+            <p>Unoverse 2</p>
+          </a>
         </div>
         <div class="card">
-          <img src="" alt="" />
-          <p>Project name</p>
+          <a :href=limbus_3>
+            <img src="../assets/images/Limbus_3.jpg" al="" />
+            <p>Limbus 3</p>
+          </a>
         </div>
       </div>
     </section>
     <section id="quote-block">
-      <p>
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus quasi
-        repellat inventore. Quis expedita necessitatibus porro eaque aut magni
-        vel praesentium sequi enim aspernatur molestiae, tempore possimus quidem
-        et! Rerum!"
-      </p>
-    </section>
-    <section id="sign-up-block">
-      <div id="sign-up-banner">
-        <div class="left">
-          <h5>Yah diiig!</h5>
-          <p>Sign Up for more UnoTheActivist content</p>
+      <div class="wrapper">
+        <div>
+          <p>
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus quasi
+            repellat inventore. Quis expedita necessitatibus porro eaque aut magni
+            vel praesentium sequi enim aspernatur molestiae, tempore possimus quidem
+            et! Rerum!"
+          </p>
         </div>
-        <div class="right">
-          <button>Sign Up</button>
+        <div id="quoter">
+          <p>-UnoTheActivist</p>
         </div>
       </div>
     </section>
+    <div class="wrapper">
+      <section id="sign-up-block">
+        <div id="sign-up-banner">
+          <div class="left">
+            <h5>Yah diiig!</h5>
+            <p>Sign Up for more UnoTheActivist content</p>
+          </div>
+          <div class="right">
+            <button>Sign Up</button>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const showStopper = ref("https://music.youtube.com/playlist?list=OLAK5uy_lePiipMg8WRQQVz5qdTd3rOywRyU8kQ4s&feature=share")
+const unoverse = ref("https://music.youtube.com/playlist?list=OLAK5uy_kYuDdvgbN1pLOEGB5QDDKmMfkYX4HGLbE&feature=share")
+const unoverse_two = ref("https://music.youtube.com/playlist?list=OLAK5uy_kcx-TN73KY2nOmRKJ7HK9BraaQlbD-58s&feature=share")
+const limbus_3 = ref("https://music.youtube.com/playlist?list=OLAK5uy_kAhPGIIjKniGq2jMNgKOoXlPgrydOvc-w&feature=share")
+</script>
 
 <style scoped>
 .main-section {
@@ -61,32 +86,41 @@
   font-weight: 800;
   color: #1f2937;
   text-align: center;
+  /* padding: 0.5rem; */
   margin-bottom: 1.5rem;
 }
 
 .random-info-block {
-  padding: 1.5rem 0;
+  padding: 3rem 0 4.8rem 0; 
 }
 
 img {
-  height: 100px;
-  width: 100px;
+  height: 150px;
+  width: 150px;
   border: solid 2px #3882f6;
-  border-radius: 0.5rem;
+  border-radius: 0.4rem;
   text-align: center;
 }
 
-.card {
+.card , .card > p{
   text-align: center;
+  font-size: 16px;
+  font-weight: 600;
 }
 
 .cards-container {
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  gap: 5rem;
+  padding: 1rem 0;
 }
 
 #quote-block {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
   background-color: #e5e7eb;
+  padding: 6rem;
   color: #1f2937;
   font-size: 36px;
   font-weight: lighter;
@@ -116,6 +150,17 @@ img {
 .left > p {
   color: #e5e7eb;
   font-size: 10px;
+}
+
+a{
+    color: #1f2937;
+}
+
+#quoter{
+  display: flex;
+  justify-content: flex-end;
+  font-size: 30px;
+  font-weight: 700;
 }
 
 button {
